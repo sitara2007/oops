c2a63ab9664a625d042c6936881b3a51847e20fd
sitara2007 <pishu97823@gmail.com>
2026-06-04 15:17:55 +0530

    update decorator logic

--- decorator.py
+++ decorator.py
@@ -17,16 +17,25 @@ print(obj.getter())
 #3. same_propertyname.setter
 #4. def sameproperty_name(self, new_value) and 
 #5. then self.__property name=newvalue
+
+#example-->
+#create a class nme bank account inside that acc num and balance will be the property and define getter and setter method to access those paticular values and set a new value
+
 class bank_account:
-    def __init__(self,balance):
+    def __init__(self,balance, acc_num):
         self.__balance = balance
+        self.__acc_num=acc_num
     @property
     def getter(self):
-        return self.__balance
+        return self.__balance,self.__acc_num
     @getter.setter
-    def setter(self,value):
-        self.__balance = value
-obj=bank_account(1000)
+    def setter(self,value1,value2):
+        self.__balance = value1
+        self.__acc_num=value2
+obj=bank_account(1000,123456)
 print(obj.getter)
-obj.setter=2000
+obj.setter(200,400)
 print(obj.setter)
+print(obj.getter)
+print("program successfully executed")
+#program successfully executed
