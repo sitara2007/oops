b2dac54c078e1bc05ce860b2829615d3b6daee85
sitara2007 <pishu97823@gmail.com>
2026-06-04 15:57:24 +0530

    Cleaned up licenses and updated OOPS files

--- abstraction.py
+++ abstraction.py
@@ -1,2 +1,7 @@
 #1. using abstract base class (abc) 
 #2. using abstractmethod decorator like @abstractmethod use pass inside the function in abs method 
+from abc import ABC, abstractmethod
+class parent(ABC):
+    @abstractmethod
+    def area(self):
+        pass #abstraction method, no implementation here
