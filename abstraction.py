da84ef25c050406f34a7ab7532c9a785669b83e6
sitara2007 <pishu97823@gmail.com>
2026-06-04 16:15:13 +0530

    added abstraction using ABC method

--- abstraction.py
+++ abstraction.py
@@ -1,7 +1,17 @@
-#1. using abstract base class (abc) 
+#1. using abstract base class (ABC) 
 #2. using abstractmethod decorator like @abstractmethod use pass inside the function in abs method 
 from abc import ABC, abstractmethod
-class parent(ABC):
+class child(ABC):
     @abstractmethod
-    def area(self):
+    def area(self, name,roll):
         pass #abstraction method, no implementation here
+class child1(child):
+    def area(self,name, roll):
+        self.name=name
+        self.roll-roll
+        return f" name is {self.name} and roll is {self.roll}"
+        #implementation of the abstract method
+        
+obj=child1()
+print(obj.area("meena",1234))
+#program successfully executed
