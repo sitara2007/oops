741c6d16688620e2b2b3cd9ce83fc4fe8555bb4c
sitara2007 <pishu97823@gmail.com>
2026-06-13 23:18:42 +0530

    Cleanup: Permanently remove oopsII folder

--- oopsII/closure.py
+++
@@ -1,9 +0,0 @@
-def main_welcome():
-    msg='welcome to capgemini'
-    def inner_welcome():
-        print('study python')
-        print('msg')
-        print('lets learn langchain')
-    return inner_welcome()
-main_welcome()
-#program successfully executed
